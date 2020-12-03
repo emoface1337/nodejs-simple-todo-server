776be42a739326abccaecf6d4566c49ffcc3defd
emoface1337 <chvrches@mail.ru>
2020-12-03 16:56:49 +0300

    editing & deleting functionality, some refactoring

--- models/task.js
+++ models/task.js
@@ -1,26 +1,30 @@
 const { Schema, model } = require('mongoose')
 
 const task = new Schema({
-    id: {
-        type: String,
-        required: true
+        id: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        created: {
+            type: Number,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            required: true
+        },
+        favorite: {
+            type: Boolean,
+            required: true
+        }
     },
-    text: {
-        type: String,
-        required: true
-    },
-    created: {
-        type: Number,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        required: true
-    },
-    favorite: {
-        type: Boolean,
-        required: true
+    {
+        versionKey: false
     }
-})
+)
 
 module.exports = model('Task', task, 'todo-app')
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,37 +2,68 @@ const { Router } = require('express')
 const Task = require('../models/task')
 const router = Router()
 
+const successResponseBody = {
+    statusCode: 0
+}
+
 router.get('/', async (req, res) => {
 
     const tasks = await Task.find()
     res.send({
         tasks: tasks,
-        statusCode: 0
+        ...successResponseBody
     })
+
 })
 
 router.post('/', async (req, res) => {
 
-    const task = new Task({
+    const task = {
         id: req.body.id,
         text: req.body.text,
         created: req.body.created,
         completed: req.body.completed,
         favorite: req.body.favorite
-    })
-
-    try {
-        await task.save()
-        res.send({ statusCode: 0 })
-    } catch (e) {
-        res.send(e)
     }
+
+    await Task.create(task, (error, result) => {
+        error
+            ? res.send({ statusCode: 10, message: error.message })
+            : res.send({ ...successResponseBody, result })
+    })
 
 })
 
 router.get('/:id', async (req, res) => {
+
     const task = await Task.findOne({ id: req.params.id })
     res.json(task)
+
+})
+
+router.put('/:id', async (req, res) => {
+
+        const { id } = req.params
+        delete req.body.id
+
+        await Task.updateOne({ id: id }, req.body, null, (error, result) => {
+            error
+                ? res.send({ statusCode: 10, message: error.message })
+                : res.send({ ...successResponseBody, result })
+        })
+    }
+)
+
+router.delete('/:id', async (req, res) => {
+
+    const { id } = req.params
+
+    await Task.deleteOne({ id: id }, null, (error) => {
+        error
+            ? res.send({ statusCode: 10, message: error.message })
+            : res.send({ ...successResponseBody })
+    })
+
 })
 
 module.exports = router
